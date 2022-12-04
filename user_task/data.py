73cd65dd1fc4f0269788093e7f9049a2f92fb34c
korsh624 <korsh624@gmail.com>
2022-12-04 23:58:07 +0300

    add canny

--- user_task/data.py
+++ user_task/data.py
@@ -39,14 +39,14 @@ pictur=findxywh(img,mask_min,mask_max)
 # for i in range(len(pictur)):
 #     cv2.imshow(str(i),pictur[i])
 item_pictur=[]
-cv2.imshow('w',pictur[0])
+# cv2.imshow('w',pictur[0])
 mask_min_item=[(118,158,162),(52,182,204),(189,250,223)]
 mask_max_item=[(101,87,94),(36,109,153),(170,184,180)]
 hsv_item = cv2.cvtColor(pictur[0], cv2.COLOR_BGR2HSV)
 hsv_item=cv2.GaussianBlur(hsv_item, (15, 15), 2)
 thresh_item = cv2.inRange(hsv_item, mask_min_item[1], mask_max_item[1])
 contours0_item, hierarchy = cv2.findContours(thresh_item.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.imshow('filter', thresh_item)
+# cv2.imshow('filter', thresh_item)
 for cnt in contours0_item:
     rect = cv2.minAreaRect(cnt)
     box = cv2.boxPoints(rect)
@@ -54,8 +54,33 @@ for cnt in contours0_item:
     print(len(box))
     box = np.int0(box)
     cv2.drawContours(img, [box], -1, (255, 0, 0), 0)
+def getcontours(img):
+    imgContour=img.copy()
+    contours,mierchy=cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    if contours:
+        for cnt in contours:
+            area=cv2.contourArea(cnt)
+            print(area)
+            if area>500:
+                cv2.drawContours(imgContour,cnt,-1,(255,0,0),3)
+                per1=cv2.arcLength(cnt,True)
+                approx=cv2.applyColorMap(cnt,0.02*per1,True)
+                print(len(approx))
+                objCor=len(approx)
+                x,y,w,h=cv2.boundingRect(approx)
+                cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+                cv2.imshow('cont',imgContour)
+                if objCor==4:
+                    print(objCor)
+                    return
+
 
 cv2.imshow("frame",img)
 cv2.imshow('contours', pictur[0])
+img_gray=cv2.cvtColor(pictur[0],cv2.COLOR_BGR2GRAY)
+img_blur=cv2.GaussianBlur(img_gray,(7,7),1)
+imgCanny=cv2.Canny(img_blur,50,50)
+angle_4=getcontours(imgCanny)
+print(angle_4)
 
 cv2.waitKey(0)
